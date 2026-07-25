a8c335fc5ab19d9e4f624bd8da1d8af3d331f79c
sudhanshu ranjan <166327067+Mithu699@users.noreply.github.com>
2026-07-25 20:13:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectToDB = require("./database/db");
+
+// connect to our database
+
+connectToDB();
+
+// middleware -> express.json()
+app.use(express.json())
+
+app.listen(PORT, () => {
+console.log(`Server is  running on ${PORT}`);
+
+})
